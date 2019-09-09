1716025e50cebdf798a05e0cbcdada315224fb1a
Jack Clarke <me@jackclarke.xyz>
2019-09-09 12:41:58 +0100

    moar bordars

--- config.def.h
+++ config.def.h
@@ -6,7 +6,7 @@
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
 static char *font = "Consolas:pixelsize=17:antialias=true:autohint=true";
-static int borderpx = 5;
+static int borderpx = 15;
 
 /*
  * What program is execed by st depends of these precedence rules:
