042ea39ca838221f91a6ba91605cce140ca516e5
Jack Clarke <jackojc@gmail.com>
2022-04-28 15:16:35 +0100

    Change font to APL385

--- config.def.h
+++ config.def.h
@@ -5,7 +5,7 @@
  *
  * font: see http://freedesktop.org/software/fontconfig/fontconfig-user.html
  */
-static char *font = "JetBrains Mono:pixelsize=15:antialias=true:autohint=true";
+static char *font = "APL385 Unicode:pixelsize=17:antialias=true:autohint=true";
 static int borderpx = 5;
 
 /*
